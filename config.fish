2c160e2f3d36127a048e878c6a67f6e361d76d22
João Baptista <maybejoao@gmail.com>
2023-09-05 13:30:35 +0100

    Add config

---
+++ config.fish
@@ -0,0 +1,28 @@
+if status is-interactive
+    fish_vi_key_bindings
+    bind -M insert -m default jk backward-char force-repaint
+    set -g EDITOR "nvim"
+    fish_add_path -m "/opt/homebrew/bin"
+
+    alias ll='ls -l'
+    alias vi='vim'
+    alias vim='nvim'
+    alias vimrc='nvim ~/.config/nvim/init.lua'
+    alias vimplug='nvim ~/.config/nvim/lua/joao/plugins.lua'
+    alias shrc='nvim ~/.config/fish/config.fish; . ~/.config/fish/config.fish'
+
+    # Rust
+    set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
+
+    # Node 
+    set --universal nvm_default_version v18.17.0
+
+    # Ruby
+    fish_add_path "/opt/homebrew/opt/ruby@2.7/bin"
+
+    # Python
+    set -g PYENV_ROOT "$HOME/.pyenv"
+    fish_add_path "$PYENV_ROOT/bin"
+    pyenv init - | source
+    fish_add_path -m /Users/joao/.local/bin
+end
